f564169f37ed411a193d027955f8274d14e60abe
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2011-06-22 13:15:56 +0200

    Add examples to demo execute_file

---
+++ Manatee.Test/DB/005 - Create Category.sql
@@ -0,0 +1,2 @@
+﻿INSERT INTO categories (title, description, ModifiedBy, ModifiedOn, CreatedBy, CreatedOn)
+VALUES ('Default', 'default category', 'deploy', getdate(), 'deploy', getdate())
---
+++ Manatee.Test/DB/005 - Drop Category.sql
@@ -0,0 +1,1 @@
+﻿DELETE FROM categories WHERE ModifiedBy = 'deploy'
