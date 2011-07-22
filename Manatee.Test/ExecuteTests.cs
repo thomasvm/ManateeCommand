658d486c9bbf3df3c22392e3bfe4ca19657b7fa1
Thomas Van Machelen <thomas.vanmachelen@gmail.com>
2011-07-22 18:55:12 +0200

    Add support for execute arrays (for better formatting)

---
+++ Manatee.Test/ExecuteTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Manatee.Test
+{
+    [TestFixture]
+    public class ExecuteTests
+    {
+        [Test]
+        public void Execute_WithArray_ConcatenesValues()
+        {
+            var migrator = new Migrator();
+
+            string json = @"{execute: [
+                                'SELECT current_timestamp, ',
+                                'system_user'
+                            ]}";
+
+            string command = migrator.GetCommandFromJson(json);
+
+            Assert.AreEqual(string.Format(@"SELECT current_timestamp, {0}system_user{0}", Environment.NewLine), command);
+        }
+
+        [Test]
+        public void Execute_WithSingleValue_TakesValue()
+        {
+            var migrator = new Migrator();
+
+            string json = @"{execute: 'SELECT current_timestamp' }";
+
+            string command = migrator.GetCommandFromJson(json);
+
+            Assert.AreEqual("SELECT current_timestamp", command);
+        }
+    }
+}
